cc75f12d7463249e7420b6e685911a8ac39bc13f
chanchalkumawat <kumawat.chanchal03@gmail.com>
2017-02-28 08:56:08 +0530

    First commit here

---
+++ VZWCommonActorConstants.java
@@ -0,0 +1,17 @@
+package com.vzw.common.constants;
+
+/**
+ * The Class VZWCommonActorConstants.
+ */
+public class VZWCommonActorConstants {
+	public static final String EXPRESS_ADD_UPDATE_DEVICE_ROUTER = "expressAddUpdateDeviceRouter";
+	public static final String GRIDWALL_ROUTER = "gridWallRouter";
+	public static final String MTN_DETAIL_ROUTER = "mtnDetailRouter";
+	public static final String PDP_ROUTER = "pdpRouter";
+	public static final String TRADEIN_ROUTER = "tradeinRouter";
+	public static final String CQ_ROUTER = "cqRouter";
+	/**
+	 * Instantiates a new VZW common actor constants.
+	 */
+	private VZWCommonActorConstants() {}
+}
